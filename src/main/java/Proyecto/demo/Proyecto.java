52648512dc15f21f125fade987195442ae1448f9
naya2904 <nayarit2904@gmail.com>
2022-09-26 20:41:39 -0600

    Primer commit del proyecto

---
+++ src/main/java/Proyecto/demo/Proyecto.java
@@ -0,0 +1,13 @@
+package Proyecto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto.class, args);
+	}
+
+}
